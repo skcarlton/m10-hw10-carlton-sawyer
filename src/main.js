8d7835cd0c91a9e43fcf55431f933650bc1e620e
Sawyer Carlton <skcarlton@gmail.com>
2021-03-21 23:03:03 -0400

    Show temp message

--- src/main.js
+++ src/main.js
@@ -6,8 +6,19 @@ const freezingTemp = document.getElementById("freezing-temp");
 boilingTemp.innerHTML = "212";
 freezingTemp.innerHTML = "32";
 
+
 //Water temp
 
 const waterTemp = document.getElementById("water-temp");
 
 waterTemp.innerHTML = "100";
+
+
+//Temp message
+
+const goodTemp = document.getElementById("good-temp");
+
+setTimeout( function() {
+    goodTemp.classList.remove("hide");
+    goodTemp.classList.add("show");
+}, 3000)
